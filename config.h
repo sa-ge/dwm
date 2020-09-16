cb85b8c89f568671fa94ac658cee333288470e0a
alhelaly <alhe22lali@gmail.com>
2020-09-16 22:36:29 +0300

    -

--- config.h
+++ config.h
@@ -56,7 +56,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "osxmanager",     NULL,       NULL,       2,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
